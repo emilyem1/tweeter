df4d6336fba38b0c4943d6b0c43540d95f3d9dba
Emily McMahon <emilyemcmahonn@gmail.om>
2023-09-17 21:29:00 -0700

    Eslint code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
-// For the counter behaviour: 
+// For the counter behaviour:
 
 $(document).ready(function() {
   $(".new-tweet textarea").on('input', function() {
     // Wrap 'this' in a jQuery object
-    var textareaValue = $(this).val();
-    var length = textareaValue.length;
-    var charactersLeft = 140 - length;
+    const textareaValue = $(this).val();
+    const length = textareaValue.length;
+    let charactersLeft = 140 - length;
     // Find the counter element by traversing the DOM tree
-    var counter = $(this).closest('form').find('.counter');
+    const counter = $(this).closest('form').find('.counter');
     // Update the counter text
     counter.text(charactersLeft);
-        if (charactersLeft < 0) {
+    if (charactersLeft < 0) {
       counter.addClass('red-text');
     } else {
       counter.removeClass('red-text');
