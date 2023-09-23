d5ce64864e4a32a252c3ec3c92b862c62f2f1184
Emily McMahon <emilyemcmahonn@gmail.om>
2023-09-22 21:28:41 -0700

    Update images for avatars to make it on theme

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -29,8 +29,8 @@ module.exports = {
    
     const avatars = {
     
-      Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
-      Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
+      Female: ["/images/female-1.png","/images/female-2.png","/images/female-3.png","/images/female-4.png"],
+      Male: ["/images/male-1.png","/images/male-2.png","/images/male-3.png","/images/male-4.png"]
     
     }
     
